5b04f4d7d302c202a0ae0c88af27cb9afc65a7d9
Tetiana Pavlyshyn <example@email.com>
2023-05-12 22:55:00 +0300

    Last commit

--- service/src/test/java/com/epam/esm/service/impl/GiftCertificateServiceImplTest.java
+++ service/src/test/java/com/epam/esm/service/impl/GiftCertificateServiceImplTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.jpa.domain.Specification;
